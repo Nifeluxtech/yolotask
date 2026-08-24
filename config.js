6bbba4426c16897b4a311cbf153e5fad442fff88
Nifeluxtech <oluwanifemiolude682@gmail.com>
2026-08-24 18:18:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.YOLOTASK_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
-  paystackPublicKey: ''
+  supabaseUrl: 'https://dqfpeeakobucyaeiqguc.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZnBlZWFrb2J1Y3lhZWlxZ3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1ODQ3OTUsImV4cCI6MjEwMzE2MDc5NX0.-JStEsWIYOgL0qlP0NB2TPGJzTrk7ULW96qCwh20FNw',
+  paystackPublicKey: 'pk_live_cd8547f6b4270551729c247d8d31635691c39a08'
 };
